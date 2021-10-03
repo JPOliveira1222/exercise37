6c63d6fd31c9e6235c2d92d1ee47a389109786e2
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:56:50 -0400

    pseudocode for 37

---
+++ src/main/java/exercise37/solution37.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 37 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+package exercise37;
+
+import java.util.*;
+
+public class solution37 {
+
+    /*
+    print "what's the minimum length"
+    length = input from user that is turned into an int
+
+    print "How many special characters?"
+    character = input from user that is turned into int
+
+    print "how many numbers?"
+    numbers = input from user that is turned into an int
+
+    print "your password is " randomPassword that takes in the user inputs of length, special characters and numbers.
+
+    using a function that created a random password
+    set the letters, numbers, and special characters that can be used
+
+    add random letters from the predetermined string depending on the length of the password
+
+    add random special characters from the predetermined string and amount of special characters requested by the user
+
+    add random numbers from the predetermined string and amount of numbers requested by the user
+
+    put it all together and shuffle them in a random order
+
+    print out the completed password.
+
+
+     */
+
+    
+
+}
